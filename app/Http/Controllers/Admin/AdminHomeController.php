40ef9af2425c1e1087ceefab1b9906ed2200c112
Carlos Aravena <cfranciscoj@hotmail.com>
2025-06-26 14:02:15 -0400

    .

--- app/Http/Controllers/Admin/AdminHomeController.php
+++ app/Http/Controllers/Admin/AdminHomeController.php
@@ -8,4 +8,23 @@ use Illuminate\Http\Request;
 class AdminHomeController extends Controller
 {
     //
+        use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+        $this->middleware('auth')->only('logout');
+    }
 }
